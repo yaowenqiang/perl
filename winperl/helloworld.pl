747aee2db946b0979162e1cf8e1c4be6e087b27e
jack yao <yaowenqiang111@163.com>
2014-05-22 23:10:08 +0800

    add the helloworld pl demo file

--- winperl/helloworld.pl
+++ winperl/helloworld.pl
@@ -1,3 +1,29 @@
 #!/bin/perl
 # Print our first message
-print "Hello world"
+$message = "hello";
+#print "Hello world"
+$fname = 'dean';
+$lname = 'davis';
+#concatenate $fname & $lname
+#$full_name = $fname.'.'.$lname;
+@full_name = ('dean','davis');
+@full_name = ('dean','davis','jack yao');
+#print("Full Name Arrays:");
+#print $full_name,"\n";
+print "Full Name Arrays $full_name[0]","\n";
+print "Full Name Arrays $full_name[1]","\n";
+#define count of array elements
+$count = $#full_name + 1;
+print "Array full_name contains ",$#full_name+1," elements \n";
+print "Array full_name contains $count"," elements \n";
+#print $lname,"\n";
+
+#print("$message - $fname\n");
+#print("$message ",' - ',$fname);
+#Hash examples
+
+%leader_country = (
+    Obama => 'USA',
+    'Cameron' => 'United Kingdom',
+);
+print "$leader_country{'Cameron'}"
